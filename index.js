3664b72cf4fbeb842c70c76ec2531fc5de3679d9
Amanda King <amapickell@gmail.com>
2019-04-30 01:55:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,24 +2,16 @@ function shout(string) {
   return string.toUpperCase();
 }
 
-shout('hello');
-
 function whisper(string) {
   return string.toLowerCase();
 }
-
-whisper('HELLO');
 
 function logShout(string) {
   string=string.toUpperCase();
   console.log(string);
 }
 
-logShout('hello');
-
 function logWhisper(string) {
   string=string.toLowerCase();
   console.log(string);
 }
-
-logWhisper('HELLO');
